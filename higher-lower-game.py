762b5b78240c6a40493ad4c46e362cf97f7263d9
meganlynn21 <meganlkeyes0203@gmail.com>
2022-04-22 21:03:07 -0400

    Put all of the code in the function main so I use the while loop at the bottom to loop back and re run it if the user picks the correct option which is either A or B. Added a count that increments on every loop.

--- higher-lower-game.py
+++ higher-lower-game.py
@@ -2,57 +2,57 @@ from art import logo
 from art import vs
 from game_data import data
 import random
+import os
 
-choice1 = data
-choice2 = data
-
-
-# Retrieve random values from game_data dictionary for guess 1 and guess 2
-def guess1(one):
-    one = random.choice(data)
-    return one
+score = 0
 
 
-def guess2(two):
-    two = random.choice(data)
-    return two
+def main():
+    # Retrieve random values from game_data dictionary
+    first_choice = random.choice(data)
+    second_choice = random.choice(data)
+    # The two choices cannot be the same
+    if first_choice == second_choice:
+        return main()
+    print(logo)
+    print("Which of the following has more followers?")
+    print(f"Compare A: {first_choice['name']}, a {first_choice['description']}, from {first_choice['country']}")
+    print(vs)
+    print(f"Against B: {second_choice['name']}, a {second_choice['description']}, from {second_choice['country']}")
+    a_or_b = input("Who has more followers? Type 'A' or 'B': ")
+    os.system('cls' if os.name == 'nt' else 'clear')
 
+    # Compare the amount of followers from the list of dictionaries and
+    # take string input as first choice A or second choice B
+    def compare(string):
+        a_choice = "A"
+        b_choice = "B"
+        if string == a_choice and first_choice['follower_count'] > second_choice['follower_count']:
+            print(f"A is greater {first_choice['follower_count']} {second_choice['follower_count']}")
+            print("Your Right!")
+            return True
+        elif string == a_choice and first_choice['follower_count'] < second_choice['follower_count']:
+            print(f"B is greater {first_choice['follower_count']} {second_choice['follower_count']}")
+            return False
+        elif string == b_choice and first_choice['follower_count'] < second_choice['follower_count']:
+            print(f"B is greater {first_choice['follower_count']} {second_choice['follower_count']}")
+            print("Your Right!")
+            return True
+        elif string == b_choice and first_choice['follower_count'] > second_choice['follower_count']:
+            print(f"A is greater {first_choice['follower_count']} {second_choice['follower_count']}")
+            return False
+        else:
+            main()
 
-"""Compare the amount of followers from the list of dictionaries and 
-take string input as first choice A or second choice B"""
+    # Create a count variable to hold the score and create a loop to keep the game going
 
-
-def compare(string):
-    if string == "A" and first_choice['follower_count'] > second_choice['follower_count']:
-        print(f"A is greater {first_choice['follower_count']} {second_choice['follower_count']}")
-        return "Your right!"
-    if string == "A" and first_choice['follower_count'] < second_choice['follower_count']:
-        print(f"B is greater {first_choice['follower_count']} {second_choice['follower_count']}")
-        return "You Lose!"
-
-    if string == "B" and first_choice['follower_count'] < second_choice['follower_count']:
-        print(f"B is greater {first_choice['follower_count']} {second_choice['follower_count']}")
-        return "Your Right!"
-    if string == "B" and first_choice['follower_count'] > second_choice['follower_count']:
-        print(f"A is greater {first_choice['follower_count']} {second_choice['follower_count']}")
-        return "You Lose!"
+    while compare(a_or_b):
+        global score
+        score += 1
+        print(f"Your score is : {score}")
+        main()
     else:
-        print(f"neither is right {first_choice['follower_count']} {second_choice['follower_count']}")
-        return "no"
+        print("YOU LOSE")
 
 
-# If user selects correct input then print this above "You're right! Current score: (put score here)" the first compare
-
-# Create a count variable to hold the score and create a loop to keep the game going
-
-first_choice = guess1(choice1)
-second_choice = guess2(choice2)
-
-print(logo)
-print("Which of the following has more followers or are same?")
-print(f"Compare A: {first_choice['name']}, a {first_choice['description']}, from {first_choice['country']}")
-print(vs)
-print(f"Against B: {second_choice['name']}, a {second_choice['description']}, from {second_choice['country']}")
-a_or_b = input("Who has more followers? Type 'A' or 'B': ")
-
-print(compare(a_or_b))
+main()
